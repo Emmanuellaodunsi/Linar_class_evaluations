109be9fc8c7373e7c9bf56de7f5541cd8d51b1d5
Emmanuellaodunsi <145551431+Emmanuellaodunsi@users.noreply.github.com>
2023-10-10 03:38:02 +0100

    Create using_if_else_condition_eligible_to_vote.py

---
+++ using_if_else_condition_eligible_to_vote.py
@@ -0,0 +1,15 @@
+
+'''Write a program to check if a person is eligible to vote'''
+name=input("Enter your name:\n")
+print("Hello",name,"welcome...This program checks if you are eligible to vote")
+age=int(input("Enter your age:\n"))
+if age>=18:
+    print("Dear",name,"You are eligle to vote.")
+else:
+    print("Dear",name,"you are not eligible to vote....wait till you are 18")
+
+
+
+
+
+
