5e822f9733e84abf9d7f44d434e09b3e107e2423
Emmanuellaodunsi <145551431+Emmanuellaodunsi@users.noreply.github.com>
2023-10-15 17:07:25 +0100

    Updated_rock_paper_scissors_game

--- Rock_Paper_Scissors_game1.py
+++ Rock_Paper_Scissors_game1.py
@@ -1,18 +1,19 @@
 import random
 import time
-#imports the 'random' and 'time' modules and 
-#prints the initial message to introduce the game"""
-print("Rock......Paper......Scissors game")
-def play_game(name=input("Hello....pls enter your name:\n")):
-    print(f"Wow",name,"Well played!...That was really fun.....\nYou have got some Rock-paper-scissors skills!")
-
-
-
+"""imported the 'random' and 'time' modules"""
+"""creating a function to print out the game name,
+ask for users name and prints the game introduction"""
+def introduction():
+    print("Rock......Paper......Scissors game")
+    global name
+    name=input("Enter your name:\n").capitalize()
+    print("Hello",name,"welcome to my rock, paper and scissors game!\n")
+introduction()
 
 """Creating an instruction function,
  which returns the game's instructions as a multi-line string."""
 def instruction():
-    return '''Welcome to Rock Paper Scissors game!
+    return '''            Game instruction: 
             In this game ,you will play against another player(computer).
             Each player will choose one of the followng gestures:
             Rock.....Paper......or scissors.
@@ -54,15 +55,15 @@ Possible_gesture=["Rock","Paper","Scissors"]
 
 """Defines the get_users_gesture function,which takes input from the user to choose a gestureand provides the result based on their choice"""
 def get_users_gesture():
-    user_gesture=int(input("Now choose your gesture\nInput 1 for Rock\nInput 2 for Paper\nInput 3 for Scissors\n"))
+    user_gesture=int(input("Input 1 for Rock\nInput 2 for Paper\nInput 3 for Scissors\nGesture:"))
     if user_gesture==1:
-        print("You choose ::: Rock")
+        print(name,"choose ::: Rock\n")
         return 'Rock'
     elif user_gesture==2:
-        print("You choose ::: Paper")
+        print(name,"choose ::: Paper\n")
         return 'Paper'
     elif user_gesture==3:
-        print("You choose ::: Scissors")
+        print(name,"choose ::: Scissors\n")
         return 'Scissors'
     else:
         print("Pls choose a valid input....you can only choose between 1 to 3\n1 for Rock\n2 for Paper\n3 for Scissors")
@@ -71,24 +72,27 @@ def get_users_gesture():
 
 """Asks the user if they are a new player and if they want to read the game instructions.
 If the response is "yes," it prints the instructions, if "no" it moves to the next step"""
-instruction_question=input("Are you a new player?,Will you like to get the game instuctions?....Enter just \"yes\" or \"no\"\n")
+instruction_question=input("Are you a new player?,Will you like to read the game instuctions?....Enter just \"yes\" or \"no\"\n")
 if instruction_question.lower()=="yes":
+    print("\nSince you are a new player...take your time to read the game instructions\nGiving you 10sec to read it",name,"\n")
     print(instruction())
 else:
-    pass
+    print("Let the game begin!!....Loading.....")
 
 """This line of code introduces a countdown before the game starts using time.sleep."""
-time.sleep(5)
-print("Lets start!!!")
+time.sleep(8)
+print("\nOkay let's start the game",name,"!!!\n")
 time.sleep(1)
-print("Rock Paper Scissors")
+print("Rock Paper Scissors\n")
 time.sleep(1.5)
-print("Rock Paper Scissors")
+print("Rock Paper Scissors\n")
 time.sleep(1.5)
-print("Rock Paper Scissors")
+print("Rock Paper Scissors\n")
 time.sleep(1.5)
-
-"""Creating variables to keep track of the user and computer scores"""   
+print("choose your gesture :)",name,"\n")
+time.sleep(2)
+"""Creating variables to keep track of the user and computer scores"""  
+time.sleep(1) 
 user_score=0
 time.sleep(1)
 computer_score=0
@@ -104,11 +108,10 @@ while True:
         user_score+=1
     elif result=="Computer win!":
         computer_score+=1
-        print(f"Your score :::{user_score}\nComputer_score :::{computer_score}")
+        print(name,"score:::",user_score,"\n""\nComputer score :::",computer_score)
         play_again=input("Do you want to play another round?(yes/no)\n").lower()
-    
+
         if play_again!="yes":
-            print(f"final score is:\nYou :::{user_score}\nComputer :::{computer_score}")
+            print("\nfinal score is:\n",name,":::",user_score,"\nComputer :::",computer_score)   
+            print(f"\nWow",name,"Well played!...That was really fun.....\nYou have got some Rock-paper-scissors skills!")
             break
-"""Calling the play_game function, which prints a concluding message based on the user's name provided at the beginning"""
-play_game()
